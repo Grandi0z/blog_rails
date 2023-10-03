1a00a891baf87cbe6f575af06723f7e170fae3fb
Grandi0z <josephlkhausi@gmail.com>
2023-10-04 00:44:50 +0200

    Add properly a forein key

--- db/migrate/20231003200705_rename_acolumn.rb
+++ db/migrate/20231003200705_rename_acolumn.rb
@@ -1,5 +1,5 @@
 class RenameAcolumn < ActiveRecord::Migration[7.0]
   def change
-    rename_column :posts, :user_id, :author_id
+    add_reference :posts, :author, references: :users, null: false, foreign_key: {to_table: :users}
   end
 end
